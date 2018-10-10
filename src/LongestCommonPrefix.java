8c8a089f33aca0a41509ccacebc8f292aee6b2c3
fengyadong001 <fengyadong001@ke.com>
2018-10-10 22:57:40 +0800

    10.10

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.operations.Minus;
+
 /**
  * @program: leetcode
  * @description: leetcode第14题 Longest Common Prefix
@@ -5,4 +7,34 @@
  * @create: 2018-10-08 22:52
  **/
 public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        return longestCommonPrefix(strs, 0 , strs.length - 1);
+    }
+
+    private String longestCommonPrefix(String[] strs, int l, int r) {
+        if (l == r) {
+            return strs[l];
+        }
+        else {
+            int mid = (l + r)/2;
+            String lcpLeft =   longestCommonPrefix(strs, l , mid);
+            String lcpRight =  longestCommonPrefix(strs, mid + 1,r);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+
+    String commonPrefix(String left,String right) {
+        int min = Math.min(left.length(), right.length());
+        for (int i = 0; i < min; i++) {
+            if ( left.charAt(i) != right.charAt(i) )
+                return left.substring(0, i);
+        }
+        return left.substring(0, min);
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+
+    }
 }
