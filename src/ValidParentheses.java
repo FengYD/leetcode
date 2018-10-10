97f14e6e9a21e1a1d2f2834256e4970c23c0a027
fengyadong001 <fengyadong001@ke.com>
2018-10-10 23:36:47 +0800

    20

---
+++ src/ValidParentheses.java
@@ -0,0 +1,38 @@
+import java.util.ArrayDeque;
+
+/**
+ * @program: leetcode
+ * @description: leetcode第20题 Valid Parentheses
+ * @author: fengyadong
+ * @create: 2018-10-10 23:00
+ **/
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        boolean result = false;
+        ArrayDeque<Character> stack = new ArrayDeque<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if(stack.isEmpty()){
+                stack.addLast(s.charAt(i));
+            } else {
+                if(s.charAt(i) == ')' && stack.getLast() == '('){
+                    stack.removeLast();
+                } else if(s.charAt(i) == '}' && stack.getLast() == '{') {
+                    stack.removeLast();
+                } else if(s.charAt(i) == ']' && stack.getLast() == '['){
+                    stack.removeLast();
+                } else {
+                    stack.addLast(s.charAt(i));
+                }
+            }
+        }
+
+        result = stack.isEmpty();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ValidParentheses validParentheses = new ValidParentheses();
+        String s = "()[]{}";
+        System.out.println(validParentheses.isValid(s));
+    }
+}
