74965d3e7878f79b5abf6171f4518dd893c9f888
fengyadong <f18813108509@gmail.com>
2019-10-27 22:32:48 +0800

    留一个小尾巴

---
+++ src/java8/stream/TestStream.java
@@ -0,0 +1,11 @@
+package java8.stream;
+
+/**
+ * @author fengyadong
+ * @Description: <br/>
+ * @Date: 2019-10-14 23:24<br/>
+ */
+public class TestStream {
+    public static void main(String[] args) {
+    }
+}
--- src/socket/SocketServerExample.java
+++ src/socket/SocketServerExample.java
@@ -1,6 +1,11 @@
 package socket;
 
-import java.net.Inet4Address;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * @author fengyadong
@@ -11,5 +16,37 @@ public class SocketServerExample {
 
     public void tcpEchoServer(){
 
+    }
+
+    public static void main(String[] args) {
+        try {
+            //创建服务器套接字
+            ServerSocket server = new ServerSocket(8000);
+            System.out.println("服务器启动完毕！");
+            //等待客户端连接，返回的是已经连接到服务器的客户端socket对象
+            Socket socket = server.accept();
+            System.out.println("客户端已经连接上服务器啦！");
+            //获取客户端socket对象的输入流
+            InputStream inputStream = socket.getInputStream();
+            InputStreamReader reader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            while (true) {
+                String string = bufferedReader.readLine();
+                //如果接收到exit就退出服务器
+                if (string.equals("exit")) {
+                    break;
+                }
+                //如果接收到的不是exit，就输出接收到的内容
+                System.out.println("接受内容为：" + string);
+            }
+            System.out.println("连接断开！");
+            bufferedReader.close();
+            reader.close();
+            inputStream.close();
+            socket.close();
+            server.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
