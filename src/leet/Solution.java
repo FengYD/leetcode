c7e20741c8d06f181967c40e5fce9c2e108fd2e6
fengyadong <f18813108509@gmail.com>
2022-02-21 09:40:53 +0800

    leetcode每日一题

--- src/leet/Solution.java
+++ src/leet/Solution.java
@@ -1,5 +1,9 @@
 package leet;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author fengyadong
  * @Description: <br/>
@@ -8,32 +12,53 @@ package leet;
 
 class Solution {
 
-    public void sortColors(int[] nums) {
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
-        for (int num : nums) {
-            if (num == 0) {
-                count0++;
-            } else if (num == 1) {
-                count1++;
-            } else {
-                count2++;
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String ss = solution.pushDominoes(".L.R...LR..L..");
+        System.out.println(ss);
+    }
+
+    public String pushDominoes(String dominoes) {
+        StringBuilder str = new StringBuilder(dominoes);
+        boolean[] visited = new boolean[str.length()];
+        Arrays.fill(visited, false);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != '.') {
+                visited[i] = true;
+                list.add(i);
             }
         }
-        int index = 0;
-        while (count0 > 0) {
-            nums[index++] = 0;
-            count0--;
+
+        while (!list.isEmpty()) {
+            List<Integer> next = new ArrayList<>();
+            for (Integer i : list) {
+                if (str.charAt(i) == 'L') {
+                    if (i - 1 >= 0 && !visited[i - 1]) {
+                        if (str.charAt(i - 1) == '.') {
+                            str.replace(i - 1, i, "L");
+                        } else if (str.charAt(i - 1) == 'R') {
+                            str.replace(i - 1, i, ".");
+                        }
+                        next.add(i - 1);
+                    }
+                } else {
+                    if (i + 1 < str.length() && !visited[i + 1]) {
+                        if (str.charAt(i + 1) == '.') {
+                            str.replace(i + 1, i + 2, "R");
+                        } else if (str.charAt(i - 1) == 'L') {
+                            str.replace(i + 1, i + 2, ".");
+                        }
+                        next.add(i + 1);
+                    }
+                }
+            }
+            for (Integer i : next) {
+                visited[i] = true;
+            }
+            list = next;
         }
-        while (count1 > 0) {
-            nums[index++] = 1;
-            count1--;
-        }
-        while (count2 > 0) {
-            nums[index++] = 2;
-            count2--;
-        }
+        return str.toString();
     }
 }
 
